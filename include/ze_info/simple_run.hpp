496251dbcbd7ecba57320e343d931461f742bfdc
krzysztof leszczynski <krzysztof.leszczynski@intel.com>
2021-06-28 13:41:36 +0200

    clean up - remove unneccessary libs/dlls - update help msg with all paramteres - remove single_run and zenon single run - add paramter that disable warm-up - add post proccess action that copies dlls and kernels files into execution directory

--- include/ze_info/simple_run.hpp
+++ include/ze_info/simple_run.hpp
@@ -4,11 +4,8 @@
  * SPDX-License-Identifier: MIT
  *
  */
-
 #ifndef SIMPLE_RUN
 #define SIMPLE_RUN
 
-	int run_simple();
-	int run_simple_zenon();
-	void run_mt(int queries, int qps, int pool_size,bool multi_ccs, bool fixed_dist, bool log = false);
+    void run_mt(int queries, int qps, int pool_size,bool multi_ccs, bool fixed_dist, bool warm_up, bool log = false);
 #endif
